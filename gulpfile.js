107f36493cb03860f09fba4271ff08f9c795dcf0
Cristian J. Ambrosi <cristian.ambrosi@compasso.com.br>
2020-05-15 11:56:44 -0300

    Criado arquivo gulpfile.json e função para ler

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp          = require('gulp');
+const fs            = require('fs');
 const less          = require('gulp-less');
-// const jsmin         = require('gulp-jsmin');
 const concat        = require('gulp-concat');
 const rename        = require('gulp-rename');
 const notify        = require('gulp-notify');
@@ -12,87 +12,40 @@ const webpackConfig = require('./webpack.config.js');
 const browserSync   = require('browser-sync').create();
 
 let reload = browserSync.reload;
-let sources = {
-    less: [
-        './assets/dev/less/reset.less',
-        './assets/dev/less/default.less'
-    ],
-    lib: {
-        css: [
-            './node_modules/materialize-css/dist/css/materialize.min.css',
-            // './node_modules/@fortawesome/fontawesome-free/webfonts/*.eot',
-            // './node_modules/@fortawesome/fontawesome-free/webfonts/*.svg',
-            // './node_modules/@fortawesome/fontawesome-free/webfonts/*.ttf',
-            // './node_modules/@fortawesome/fontawesome-free/webfonts/*.woff',
-            // './node_modules/@fortawesome/fontawesome-free/webfonts/*.woff2',
-            './node_modules/@fortawesome/fontawesome-free/css/all.min.css'
-        ],
-        js: [
-            './node_modules/jquery/dist/jquery.min.js',
-            './node_modules/materialize-css/dist/js/materialize.min.js',
-            './node_modules/@fortawesome/fontawesome-free/js/all.min.js'
-            // './node_modules/google-charts/dist/googleCharts.min.js'
-        ]
-    }, 
-    js: [
-        // './assets/dev/js/calc/calculos-estatisticos.js',
-        './assets/dev/js/calc/application.js',
-        // './assets/dev/js/calc/chart.module.js',
-        './assets/dev/js/default.js'
-    ],
-    watch: {
-        less: [
-            './assets/dev/less/**/*.less'
-        ],
-        js: [
-            './assets/dev/js/**/*.js'
-        ],
-        html: [
-            './*.html'
-        ]
-    },
-    path: {
-        css: [
-            './assets/pub/css/'
-        ],
-        js: [
-            './assets/pub/js/'
-        ]
-    },
-    server: {
-        url: 'localhost:8088',
-        port: '8090',
-        server_file: 'server.js',
-        ignore: [
-            'gulpfile.js',
-            'node_modules/'
-        ]
+
+const sources = function() {
+    let jsonFile = './gulpfile.json';
+    try {
+        return JSON.parse(fs.readFileSync(jsonFile));
+    } catch (err) {
+        console.log('Error parsing JSON string:', err);
+        return;
     }
 }
 
 function libScript() {
-    return gulp.src(sources.lib.js)
+    return gulp.src(sources().lib.js)
         .pipe(concat('lib.js'))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest(sources.path.js));
+        .pipe(gulp.dest(sources().path.js));
 }
 
 function libStyle() {
-    return gulp.src(sources.lib.css)
+    return gulp.src(sources().lib.css)
         .pipe(concat('lib.css'))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest(sources.path.css));
+        .pipe(gulp.dest(sources().path.css));
 }
 
 function script() {
-    return gulp.src(sources.js)
+    return gulp.src(sources().js)
         .pipe(webpackStream(webpackConfig), webpack)
         //.pipe(debug({ title: 'webpack:' }))
-        .pipe(gulp.dest(sources.path.js));
+        .pipe(gulp.dest(sources().path.js));
 }
 
 function style() {
-    return gulp.src(sources.less)
+    return gulp.src(sources().less)
         .pipe(concat('bundle.css'))
         .pipe(less())
         .pipe(uglifycss({
@@ -100,15 +53,15 @@ function style() {
             'uglyComments': false
         }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest(sources.path.css))
+        .pipe(gulp.dest(sources().path.css))
         .pipe(browserSync.stream());
 }
 
 function server(done) {
     let called = false;
     return nodemon({
-        script: sources.server.server_file,
-        ignore: [sources.server.ignore]
+        script: sources().server.server_file,
+        ignore: [sources().server.ignore]
     }).on('start', function() {
         if (!called) {
             called = true;
@@ -119,17 +72,17 @@ function server(done) {
 
 function browserSyncServer() {
     browserSync.init({
-        proxy: sources.server.url,
-        port: sources.server.port,
+        proxy: sources().server.url,
+        port: sources().server.port,
         notify: true
     });
 }
 
 function watchFile() {
-    gulp.watch(sources.watch.less, style);
-    gulp.watch(sources.watch.html).on('change', reload);
-    gulp.watch(sources.watch.js, script).on('change', reload);
-    gulp.src(sources.path.js + 'bundle.min.js')
+    gulp.watch(sources().watch.less, style);
+    gulp.watch(sources().watch.html).on('change', reload);
+    gulp.watch(sources().watch.js, script).on('change', reload);
+    gulp.src(sources().path.js + 'bundle.min.js')
         .pipe(notify({ message: 'Gulp is Watching. Happy Coding!' }));
 }
 
